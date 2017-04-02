e254b6b61615b896b2afb7b465b4416b3530c322
RomeoST <agent2995@gmail.com>
2017-04-02 19:18:57 +0300

    Update 1

--- BookLibrary/Controllers/BookController.cs
+++ BookLibrary/Controllers/BookController.cs
@@ -13,7 +13,54 @@ namespace BookLibrary.Controllers
         // GET: Book
         public ActionResult Index()
         {
+            var test = context.Books.ToList();
             return View(context.Books.ToList());
+        }
+
+        // GET : Book/Edit
+        public ActionResult Edit(int id = -1)
+        {
+            var Book = context.Books.Find(id);
+            if (Book == null)
+                return new HttpNotFoundResult();
+
+            return View(Book);
+        }
+
+
+        // GET : Book/Add
+        public ActionResult Add()
+        {
+            var list = new List<SelectListItem>();
+            foreach(var item in context.Authors.Select(p => p.FullName))
+            {
+                list.Add(new SelectListItem() { Text = item });
+            }
+            ViewBag.Authors = list;
+            return PartialView();
+        }
+
+        // TODO: отправить свой JSON
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult Add(Book book, string selectAuthor)
+        {
+            if (book == null) return null;
+            var Author = context.Authors.First(p => p.FullName == selectAuthor);
+            if (Author == null) return null;
+
+            Author.Books.Add(book);
+
+            context.SaveChanges();
+
+            Book b = new Book();
+            b.Name = book.Name;
+            b.Id = book.Id;
+            b.Author = new Author { FullName = book.Author.FullName };
+
+            var test = Json(b, JsonRequestBehavior.AllowGet);
+
+            return Json(b, JsonRequestBehavior.AllowGet);
         }
     }
 }
